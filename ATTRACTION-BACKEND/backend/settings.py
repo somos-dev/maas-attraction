aa7f0314e2e90d8ba703b49c27b47b184dd25eb3
danielerogano <danielerogano@gmail.com>
2025-05-26 16:17:52 +0200

    allowed host added

--- ATTRACTION-BACKEND/backend/settings.py
+++ ATTRACTION-BACKEND/backend/settings.py
@@ -153,6 +153,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = ['attraction.somos.srl']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/

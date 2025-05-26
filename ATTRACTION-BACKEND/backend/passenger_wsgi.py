d2d99d4774a973a6b9bd4b1162be7a2fa0691ff4
danielerogano <danielerogano@gmail.com>
2025-05-26 20:02:56 +0200

    passenger file fixed

--- ATTRACTION-BACKEND/backend/passenger_wsgi.py
+++ ATTRACTION-BACKEND/backend/passenger_wsgi.py
@@ -1,24 +1,23 @@
-import sys
 import os
+import sys
 
-# Percorso assoluto della root del sito (httpdocs)
+# Percorso del progetto
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-
-# Percorso virtualenv fuori da httpdocs
 VENV_PATH = '/var/www/vhosts/attraction.somos.srl/venv-maas'
 
-# Aggiungi il progetto al sys.path
+# Aggiungi httpdocs al path
 sys.path.insert(0, PROJECT_PATH)
 
-# Imposta il modulo settings di Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+# Imposta settings di Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-# Attiva la virtualenv
+# Attiva virtualenv
 activate_this = os.path.join(VENV_PATH, 'bin/activate_this.py')
 if os.path.exists(activate_this):
     with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
 
-# Avvia l'app WSGI
+# Import finale obbligatorio
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
